53bc93bd1fc7a24226361b45884778b4c13af914
rssr25 <rahulsharmacs50@gmail.com>
2020-10-23 00:06:04 +0530

    JSX vs HTML

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,13 +2,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+function getButtonText(){
+
+    return 'Click On Me!'
+}
 //create a react component
 const App = () => {
-    return <div>Hi App There!</div>
+
+    return (
+        <div>
+            <label className="label" for="name">Enter Name:</label>
+            <input id="name" type="text"/>
+            <button style={{backgroundColor:'blue', color:'white'}}>
+                {getButtonText()}
+            </button>
+        </div>
+    )
 }
 
 //show that component on the screen
 ReactDOM.render(
     <App/>,
-    document.querySelector('#root')
+    document.querySelector("#root")
 )
