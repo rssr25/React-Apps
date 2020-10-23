71da6e37abaca81e6a66d28557536b63e198bf57
rssr25 <rahulsharmacs50@gmail.com>
2020-10-23 13:50:47 +0530

    using props for name

--- components/src/CommentDetail.js
+++ components/src/CommentDetail.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import faker from 'faker';
 
-const CommentDetail = () => {
-
+const CommentDetail = (props) => {
+    console.log(props)
     return (
-        
+
         <div className="comment">
                 <a href="/" className="avatar">
                     <img alt="avatar" src={faker.image.avatar()}/>
                 </a>
                 <div className="content">
                     <a href="/" className="author">
-                        Rahul
+                        {props.author}
                     </a>
                     <div className="metadata">
                         <span className="date">Today at 12:00PM</span>
@@ -20,3 +21,5 @@ const CommentDetail = () => {
             </div>
     );
 }
+
+export default CommentDetail;
--- components/src/index.js
+++ components/src/index.js
@@ -1,55 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import faker from 'faker'
+import CommentDetail from './CommentDetail'
+
 
 const App = () => {
     return(
 
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Rahul
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 12:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
-
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Rahul
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 12:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
-
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Rahul
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 12:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
+            <CommentDetail author = "Rahul"/>
+            <CommentDetail author = "Bhavya"/>
+            <CommentDetail author = "Arun"/>
         </div>
 
     );
