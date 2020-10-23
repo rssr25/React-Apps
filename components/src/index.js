38448ce79d5948b43fc5ed08d7e5e83f92037e67
rssr25 <rahulsharmacs50@gmail.com>
2020-10-23 14:30:31 +0530

    moving on to class based components

--- components/src/index.js
+++ components/src/index.js
@@ -9,27 +9,37 @@ const App = () => {
     return(
 
         <div className="ui container comments">
-            <ApprovalCard/>
-            <CommentDetail 
-                author = "Rahul" 
-                timeAgo="Today at 4:55AM" 
-                text="What a nice blog!" 
-                avatar={faker.image.avatar()}
-            />
 
-            <CommentDetail 
-                author = "Bhavya" 
-                timeAgo="Today at 2:00AM" 
-                text="Very well written!" 
-                avatar={faker.image.avatar()}
-            />
+            <ApprovalCard>
+                <h4>Warning!</h4>
+                <div>Are you sure you want to fuck that bitch up?</div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author = "Rahul" 
+                    timeAgo="Today at 4:55AM" 
+                    text="What a nice blog!" 
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
 
-            <CommentDetail 
-                author = "Arun" 
-                timeAgo="Yesterday at 5:00PM" 
-                text="I don't know I don't like it." 
-                avatar={faker.image.avatar()}
-            />
+            <ApprovalCard>
+                <CommentDetail 
+                    author = "Bhavya" 
+                    timeAgo="Today at 2:00AM" 
+                    text="Very well written!" 
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author = "Arun" 
+                    timeAgo="Yesterday at 5:00PM" 
+                    text="I don't know I don't like it." 
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
         </div>
 
     );
